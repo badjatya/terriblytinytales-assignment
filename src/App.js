935068a3d488f846f815a4d6e15c3be81ab37a61
Archit Badjatya <en19cs304010@medicaps.ac.in>
2022-12-08 20:26:32 +0530

    [Feat]: Added chart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,102 @@
-import React from "react";
+// Importing Modules
+import React, { useState } from "react";
+import Chart from "react-apexcharts";
+
+// Importing data
+import testData from "./data/test.txt";
 
 // Importing Styles
 import "./App.css";
 
 const App = () => {
+  // State
+  const [showButton, setShowButton] = useState(true);
+  const [words, setWords] = useState([]);
+  const [frequencies, setFrequencies] = useState([]);
+
+  // Fetch data function
+  const fetchData = async () => {
+    try {
+      setShowButton(false);
+      const dataBuffer = await fetch(testData);
+      const data = await dataBuffer.text();
+      findFrequencyOfWords(data);
+    } catch (error) {
+      console.log("Something went wrong while fetching data");
+      console.log(error);
+    }
+  };
+
+  // finding the frequency of occurrence of each word
+  const findFrequencyOfWords = (string) => {
+    // Replacing characters and splitting into array
+    let words = string.replace(/[,.!-?]+/g, "").split(/\s/);
+    let freqMap = {};
+    let wordFrequency = [];
+
+    // Finding frequency of words
+    words.forEach((word) => {
+      if (!freqMap[word]) {
+        freqMap[word] = 0;
+      }
+      freqMap[word] += 1;
+    });
+
+    // Converting object into array
+    Object.keys(freqMap).forEach((word) => {
+      let wordObject = { word: "", frequency: null };
+      wordObject.word = word;
+      wordObject.frequency = freqMap[word];
+      wordFrequency.push(wordObject);
+    });
+
+    // Sorting wordFrequency array in descending order
+    wordFrequency.sort((a, b) => b.frequency - a.frequency);
+
+    // Removing the empty character from the array
+    if (wordFrequency[0].word === "") {
+      wordFrequency.shift();
+    }
+
+    // making two array words and frequencies
+    let wordsArray = [];
+    let frequencyArray = [];
+    for (let i = 0; i < 20; i++) {
+      wordsArray.push(wordFrequency[i].word);
+      frequencyArray.push(wordFrequency[i].frequency);
+    }
+    setFrequencies(frequencyArray);
+    setWords(wordsArray);
+  };
+
+  // Options for chart
+  const options = {
+    chart: {
+      id: "basic-bar",
+    },
+    xaxis: {
+      categories: words,
+    },
+    colors: ["#ffc23e"],
+  };
+  const series = [
+    {
+      name: "frequency",
+      data: frequencies,
+    },
+  ];
+
   return (
     <div className="App">
-      <button className="btn">Submit</button>
+      {showButton ? (
+        <button className="btn" onClick={fetchData}>
+          Submit
+        </button>
+      ) : (
+        <div>
+          <Chart options={options} series={series} type="bar" width="1024px" />
+        </div>
+      )}
     </div>
   );
 };
